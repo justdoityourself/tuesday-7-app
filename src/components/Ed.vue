1f4d20b726dc4956d55a1e1b9ff5d901564ff793
Jacob <lifeampedup@gmail.com>
2020-04-07 19:41:11 -0600

    Update Ed.vue

--- src/components/Ed.vue
+++ src/components/Ed.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>Ed</div>
+    <div>
+        <div>{{name}}</div>
+        <v-text-field v-model="name" label="Ed's Component"></v-text-field>
+    </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name:"Ed"
+  }),
+}
+</script>
