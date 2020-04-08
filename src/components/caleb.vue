7fe7e9e6b7030a141a10902d324c9f0081eb5ba2
Jacob <lifeampedup@gmail.com>
2020-04-07 19:46:04 -0600

    Update caleb.vue
    send to mast

--- src/components/caleb.vue
+++ src/components/caleb.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>caleb</div>
+    <div>
+        <div>{{name}}</div>
+        <v-text-field v-model="name" label="caleb's Component"></v-text-field>
+    </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name:"caleb"
+  }),
+}
+</script>
