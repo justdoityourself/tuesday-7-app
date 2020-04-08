d26215c35cbe163c9fc7041b7da5ce6dbd04d70d
fredric47 <58110660+fredric47@users.noreply.github.com>
2020-04-07 19:34:26 -0600

    Update Luke.vue

--- src/components/Luke.vue
+++ src/components/Luke.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>Luke</div>
+    <div>
+        <div>{{name}}</div>
+        <v-text-field v-model="name" label="Luke's Component"></v-text-field>
+    </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name:"Luke"
+  }),
+}
+</script>
