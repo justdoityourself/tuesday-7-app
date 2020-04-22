56faaf64aab9e9e0c8d786cefc68163731eb8aa2
Jacob <lifeampedup@gmail.com>
2020-04-21 21:02:26 -0600

    Update Ed.vue

--- src/components/Ed.vue
+++ src/components/Ed.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <div>{{name}}</div>
+       <div class=gvt style='--gg:4px;--gr:1fr 1fr 1fr;--gc:1fr 1fr 1fr;width:180px;height:180px;background:lightgrey;'>
+          <template v-for="(item, index) in board">
+            <div v-if='item.x' :key=index class=gvt style='--gr:1fr;--gc:1fr;background:blue;'>X</div>
+            <div v-else-if='item.o' :key=index class=gvt style='--gr:1fr;--gc:1fr;background:green;'>O</div>
+            <div @click=turn(item) v-else :key=index class=gvt style='--gr:1fr;--gc:1fr;background:red;'>?</div>
+          </template>
+        </div> <div>{{name}}</div>
         <v-text-field v-model="name" label="Ed's Component"></v-text-field>
     </div>
 </template>
@@ -8,7 +14,21 @@
 <script>
 export default {
   data: () => ({
-    name:"Ed"
+    name:"Ed",
+    board:
+    [
+      {o:false,x:false},{o:false,x:false},{o:false,x:false},
+      {o:false,x:false},{o:false,x:false},{o:false,x:false},
+      {o:false,x:false},{o:false,x:false},{o:false,x:false}
+    ],
+    x_or_o_turn:0,
+    turn(item)
+    {
+      if(this.x_or_o_turn++ % 2)
+        item.o = true;
+      else
+        item.x = true;
+    }
   }),
 }
 </script>
