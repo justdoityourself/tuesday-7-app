7f0eb39ee941a35ee7a23072f9f589c74f76a7db
fredric47 <58110660+fredric47@users.noreply.github.com>
2020-04-21 20:48:52 -0600

    Update Luke.vue

--- src/components/Luke.vue
+++ src/components/Luke.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+       <div class=gvt style='--gg:4px;--gr:1fr 1fr 1fr;--gc:1fr 1fr 1fr;width:180px;height:180px;background:lightgrey;'>
+          <template v-for="(item, index) in board">
+            <div v-if='item.x' :key=index class=gvt style='--gr:1fr;--gc:1fr;background:blue;'>X</div>
+            <div v-else-if='item.o' :key=index class=gvt style='--gr:1fr;--gc:1fr;background:green;'>O</div>
+            <div @click=turn(item) v-else :key=index class=gvt style='--gr:1fr;--gc:1fr;background:red;'>?</div>
+          </template>
+        </div>
+
         <div>{{name}}</div>
         <v-text-field v-model="name" label="Luke's Component"></v-text-field>
     </div>
@@ -8,7 +16,21 @@
 <script>
 export default {
   data: () => ({
-    name:"Luke"
+    name:"Luke",
+     board:
+    [
+      {o:false,x:false},{o:false,x:false},{o:false,x:false},
+      {o:false,x:false},{o:false,x:false},{o:false,x:false},
+      {o:false,x:false},{o:false,x:false},{o:false,x:false}
+    ],
+    x_or_o_turn:0,
+    turn(item)
+    {
+      if(this.x_or_o_turn++ % 2)
+        item.o = true;
+      else
+        item.x = true;
+    }
   }),
 }
 </script>
