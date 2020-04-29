2f0923bb8b06daee32308bcadb4826264850a312
fredric47 <58110660+fredric47@users.noreply.github.com>
2020-04-28 21:03:19 -0600

    Update Luke.vue

--- src/components/Luke.vue
+++ src/components/Luke.vue
@@ -1,9 +1,14 @@
 <template>
 
     <div>
+
+      <template v-for="(item, index) in names">
+            <div :key=index>{{item}}</div>
+
+        </template>
       
-      <!--<p>Luke's</p>-->
-      <!--<h3>Luke's</h3>-->
+       <!--<p>Luke's</p>-->
+       <!--<h3>Luke's</h3>-->
        <div class=gvt style='--gg:4px;--gr:1fr 1fr 1fr;--gc:1fr 1fr 1fr;width:180px;height:180px;background:black;'>
           <template v-for="(item, index) in board">
             <div v-if='item.x' :key=index class=gvt style='--gr:1fr;--gc:1fr;background:darkred ;'>X</div>
@@ -21,6 +26,15 @@
 <script>
 export default {
   data: () => ({
+
+    names:
+    [
+      "#1",
+      "#2",
+      "#3",
+      "#4",
+    ],  
+
     name:"Luke",
      board:
     [
